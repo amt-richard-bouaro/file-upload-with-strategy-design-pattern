335731e2c35195f4495b451231a2be1e31fa8414
richard-bouaro <richard.bouaro@amalitech.org>
2024-06-14 15:08:43 +0000

    initial setup

---
+++ src/main/java/com/amalitech/fileuploadwithstrategydesignpattern/FileUploadWithStrategyDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.amalitech.fileuploadwithstrategydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadWithStrategyDesignPatternApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FileUploadWithStrategyDesignPatternApplication.class, args);
+  }
+
+}
